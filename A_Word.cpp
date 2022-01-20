3ef48fd000cc09102e514de8ec1b39e6b182ae10
Muhammed Ajmal <ajmalajmal.2016@gmail.com>
2022-01-20 15:37:09 +0530

    Word

--- A_Word.cpp
+++ A_Word.cpp
@@ -3,10 +3,22 @@
 int main(){
     std::string a;
     std::cin>>a;
-    int u,l;
+    int u=0,l=0;
     for (int i = 0; i < a.size(); i++)
     {
-        if(a[i])
+        if(std::isupper(a[i])) u++;
+        else l++;
     }
-    
+    if(u>l){
+        for (int i = 0; i < a.size(); i++)
+        {
+            a[i] = std::toupper(a[i]);
+        }
+    }else{
+        for (int i = 0; i < a.size(); i++)
+        {
+            a[i] = std::tolower(a[i]);
+        }
+    }
+    std::cout<<a<<'\n';
 }
