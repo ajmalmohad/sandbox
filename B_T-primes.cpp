915e58e4b3fd75b4c4588656e56748f57bdccf4c
Muhammed Ajmal <ajmalajmal.2016@gmail.com>
2022-02-13 22:57:52 +0530

    Convex Hull and Unsolved T Primes

---
+++ B_T-primes.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<cmath>
+#include<algorithm>
+#include<vector>
+using namespace std;
+typedef long long ll;
+
+ 
+
+void solve(){
+    ll n;
+    cin>>n;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
