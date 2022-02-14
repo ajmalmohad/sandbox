dc2784c691aaa6ab356094e29ef8a79fe94f050b
Muhammed Ajmal <ajmalajmal.2016@gmail.com>
2022-02-14 19:49:27 +0530

    Unsolved

---
+++ A_Cheap_Travel.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+#include<set>
+#include<cmath>
+#include<algorithm>
+using namespace std;
+typedef long long ll;
+
+void solve(){
+    ll n,m,a,b;
+    cin>>n>>m>>a>>b;
+    ll count = 0;
+
+
+    cout<<count<<'\n';
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t=1;
+    while(t--) solve();
+    return 0;
+}
