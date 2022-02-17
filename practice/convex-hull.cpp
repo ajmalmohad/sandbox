50846cd35f97e4596fdb38f6ed6022fc7314a45d
Muhammed Ajmal <ajmalajmal.2016@gmail.com>
2022-02-17 20:26:13 +0530

    Convex Hull CleanUp

--- practice/convex-hull.cpp
+++ practice/convex-hull.cpp
@@ -1,41 +1,32 @@
-
-// A C++ program to find convex hull of a set of points
 #include <bits/stdc++.h>
 using namespace std; 
-struct Point					// To store the co-ordinates of every point
-{
+struct Point{
     int x, y;
 };
+
 // To find orientation of ordered triplet (p, q, r).
-// The function returns following values
-// 0 --> p, q and r are colinear
-// 1 --> Clockwise
-// 2 --> Counterclockwise
-int orientation(Point p, Point q, Point r)
-{
-    int val = (q.y - p.y) * (r.x - q.x) -
-              (q.x - p.x) * (r.y - q.y);
-    if (val == 0) return 0; 	 // colinear
-    return (val > 0)? 1: 2; 	// clock or counterclock wise
+// 0 --> Colinear | 1 --> Clockwise | 2 --> Counterclockwise
+int orientation(Point p, Point q, Point r){
+    int val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y);
+    if (val == 0) return 0;
+    return (val > 0)? 1: 2; 
 }
-// Prints convex hull of a set of n points
-void convexHull(Point points[], int n)
-	{
-    // There must be at least 3 points
-    if (n < 3) return;
-    // Initialize Result
+
+void convexHull(Point points[], int n){
+    if (n < 3) return; // At least 3 points
     vector<Point> hull;
+
     // Find the leftmost point
     int l = 0;
-    for (int i = 1; i < n; i++)
-        if (points[i].x < points[l].x)
-            l = i;
+    for (int i = 1; i < n; i++){
+        if (points[i].x < points[l].x) l = i;
+    }
+    
     // Start from leftmost point, keep moving counterclockwise
     // until reach the start point again.  This loop runs O(h)
     // times where h is number of points in result or output.
     int p = l, q;
-    do
-    {
+    do{
         // Add current point to result
         hull.push_back(points[p]);
         // Search for a point 'q' such that orientation(p, x,
@@ -44,28 +35,24 @@ void convexHull(Point points[], int n)
         // wise point in q. If any point 'i' is more counterclock-
         // wise than q, then update q.
         q = (p+1)%n;
-        for (int i = 0; i < n; i++)
-        {
+        for (int i = 0; i < n; i++){
            // If i is more counterclockwise than current q, then
            // update q
-           if (orientation(points[p], points[i], points[q]) == 2)
-               q = i;
+           if (orientation(points[p], points[i], points[q]) == 2) q = i;
         }
         // Now q is the most counterclockwise with respect to p
         // Set p as q for next iteration, so that q is added to
         // result 'hull'
         p = q;
     } while (p != l);  // While we don't come to first point
-    // Print Result
-    for (int i = 0; i < hull.size(); i++)
-        cout << "(" << hull[i].x << ", "
-              << hull[i].y << ")\n";
+
+    for (int i = 0; i < hull.size(); i++){
+        cout << "(" << hull[i].x << ", " << hull[i].y << ")\n";
+    }
 }
-// Driver program to test above functions
-int main()
-{
-    Point points[] = {{0, 3}, {2, 2}, {1, 1}, {2, 1},
-                      {3, 0}, {0, 0}, {3, 3}};
+
+int main(){
+    Point points[] = {{0, 3}, {2, 2}, {1, 1}, {2, 1}, {3, 0}, {0, 0}, {3, 3}};
     int n = sizeof(points)/sizeof(points[0]);
     convexHull(points, n);
     return 0;
