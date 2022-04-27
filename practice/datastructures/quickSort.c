106afe7c36ae7d4d609c9c0245694ba12cda2ee9
Muhammed Ajmal <ajmalajmal.2016@gmail.com>
2022-04-27 06:53:42 +0530

    Quick sort - First and Last Element as Pivot

--- practice/datastructures/quickSort.c
+++ practice/datastructures/quickSort.c
@@ -42,7 +42,6 @@ void quickSort(int* arr, int start, int end){
 
 int main(){
     int arr[] = {9,8,7,6,5,4,3,2,1,0};
-    // int arr[] = {19,8,77,6,10,4,3,100,1,0};
     int n= sizeof(arr)/sizeof(arr[0]);
     quickSort(arr, 0,n-1);
     for(int i=0; i<n; i++) printf("%d ",arr[i]);
