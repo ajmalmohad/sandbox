84680e3259fd2bfb5d158acc32adf3168963fff4
ajmalmohad <ajmalajmal.2016@gmail.com>
2022-07-27 21:40:55 +0530

    Display and Add Via Linear Probing

--- practice/datastructures/HashTable.cpp
+++ practice/datastructures/HashTable.cpp
@@ -1,13 +1,106 @@
 #include <iostream>
 using namespace std;
 
-int hasher(string str, int capacity){
+struct node
+{
+    string key;
+    string value;
+};
+
+const int capacity = 10;
+struct node hash_table[capacity];
+
+int hasher(string str)
+{
     int value = 0;
-    for (int i = 0; i < str.size(); i++) value += (int)str[i];
+    for (int i = 0; i < str.size(); i++)
+        value += (int)str[i];
     return value % capacity;
 }
 
-int main(){
-    int capacity = 10;
-    int hash_table[capacity];
+void add(string key, string value)
+{
+    int hashed = hasher(key);
+    int current = hashed;
+    while (hash_table[current].key != "")
+    {
+        if (hash_table[current].key == key)
+        {
+            hash_table[current].value = value;
+            cout << "Added Duplicate Key! \n";
+            return;
+        }
+        current = (current + 1) % capacity;
+        if (current == hashed)
+        {
+            cout << "No space \n";
+            return;
+        }
+    }
+    hash_table[current].key = key;
+    hash_table[current].value = value;
+    cout << "Added Successfully! \n";
+}
+
+void check(string key)
+{
+}
+
+void get(string key)
+{
+}
+
+void delete_key(string key)
+{
+}
+
+void display()
+{
+    int i=0;
+    cout << "\n";
+    for (node current : hash_table)
+    {
+        cout << i++ << " " << current.key << " " << current.value << '\n';
+    }
+    cout << "\n";
+}
+
+int main()
+{
+    int option;
+    string key, value;
+    do
+    {
+        cout << "\n1. Add\n2. Check\n3. Get\n4. Delete\n5. Display\n6. Exit \n";
+        cout << "Enter your Choice: ";
+        cin >> option;
+        switch (option)
+        {
+        case 1:
+            cout << "Enter Key and Value :";
+            cin >> key >> value;
+            add(key, value);
+            break;
+        case 2:
+            cout << "Enter Key to Check :";
+            cin >> key;
+            check(key);
+            break;
+        case 3:
+            cout << "Enter Key to Get :";
+            cin >> key;
+            get(key);
+            break;
+        case 4:
+            cout << "Enter Key to Delete :";
+            cin >> key;
+            delete_key(key);
+            break;
+        case 5:
+            display();
+            break;
+        default:
+            break;
+        }
+    } while (option != 6);
 }
