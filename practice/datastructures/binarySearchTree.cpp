c49cdaa3a4dec777b40229877f11ea074cb7313e
ajmalmohad <ajmalajmal.2016@gmail.com>
2022-08-10 23:16:39 +0530

    BST in CPP

---
+++ practice/datastructures/binarySearchTree.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+Node* getNewNode(int data){
+    Node* node = new Node();
+    node->data = data;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
+
+Node* insert(Node* root, int data){
+    if(root == NULL){
+        root = getNewNode(data);
+    }else if(data < root->data){
+        root->left = insert(root->left, data);
+    }else if(data > root->data){
+        root->right = insert(root->right, data);
+    }
+    return root;
+}
+
+int getNodeCount(Node* root){
+    if(root == NULL) return 0;
+    return getNodeCount(root->right) + getNodeCount(root->left) + 1;
+}
+
+void inOrder(Node* root){
+    if(root == NULL) return;
+    inOrder(root->left);
+    cout<<root->data<<" ";
+    inOrder(root->right);
+}
+
+void preOrder(Node* root){
+    if(root == NULL) return;
+    cout<<root->data<<" ";
+    inOrder(root->left);
+    inOrder(root->right);
+}
+
+void postOrder(Node* root){
+    if(root == NULL) return;
+    inOrder(root->left);
+    inOrder(root->right);
+    cout<<root->data<<" ";
+}
+
+void deleteNodes(Node* root){
+    if(root == NULL) return;
+    deleteNodes(root->left);
+    deleteNodes(root->right);
+    delete root;
+}
+
+Node* deleteTree(Node* root){
+    deleteNodes(root);
+    return NULL;
+}
+
+bool isInTree(Node* root, int data){
+    if(root == NULL) return false;
+    else if(data < root->data) return isInTree(root->left,data);
+    else if(data > root->data) return isInTree(root->right,data);
+    else return true;
+}
+
+Node* getNode(Node* root, int data){
+    if(root == NULL) return NULL;
+    else if(data < root->data) return getNode(root->left,data);
+    else if(data > root->data) return getNode(root->right,data);
+    else return root;
+}
+
+int getMin(Node* root){
+    if(root->left == NULL) return root->data;
+    return getMin(root->left);
+}
+
+int getMax(Node* root){
+    if(root->right == NULL) return root->data;
+    return getMax(root->right);
+}
+
+bool isBSTHelper(Node* root, int lower, int upper){
+    if(root == NULL) return true;
+    if( isBSTHelper(root->left, lower, root->data) &&
+        isBSTHelper(root->right, root->data, upper) &&
+        root->data < upper && root->data > lower
+    ) return true;
+    return false;
+}
+
+bool isBST(Node* root){
+    return isBSTHelper(root, INT32_MIN, INT32_MAX);
+}
+
+int main(){
+    Node* root = NULL;
+    root = insert(root, 20);
+    root = insert(root, 8);
+    root = insert(root, 10);
+    root = insert(root, 5);
+    root = deleteTree(root);
+    root = insert(root, 12);
+    root = insert(root, 6);
+    root = insert(root, 3);
+    root = insert(root, 18);
+    root = insert(root, 21);
+    root = insert(root, 9);
+    root = insert(root, -21);
+    cout<<"\nInorder :"; inOrder(root);
+    cout<<"\nPreOrder :"; preOrder(root);
+    cout<<"\nPostOrder :"; postOrder(root);
+    cout<<"\nNodeCount: "<<getNodeCount(root);
+    cout<<"\nIs 10 in Tree ? "<<isInTree(root, 18);
+    cout<<"\nMinimum Value: "<<getMin(root);
+    cout<<"\nMaximum Value: "<<getMax(root);
+    cout<<"\nIs this a BST ? "<<isBST(root);
+    cout<<"\n";
+}
