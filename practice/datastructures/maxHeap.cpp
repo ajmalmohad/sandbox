c7e014710fe12b7a97e9a1aef7f336e804bb58d3
ajmalmohad <ajmalajmal.2016@gmail.com>
2022-08-13 10:53:43 +0530

    MaxHeap Insert and Sift Up

---
+++ practice/datastructures/maxHeap.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> heap;
+
+void swap(int a, int b){
+        int temp = heap[a];
+        heap[a] = heap[b];
+        heap[b] = temp;
+}
+
+int parent(int index){
+    return (index - 1)/2;
+}
+
+int left(int index){
+    return (2*index)+1;
+}
+
+int right(int index){
+    return (2*index)+2;
+}
+
+void siftUp(int index){
+    int elementIndex = index;
+    int parentIndex = (elementIndex - 1)/2;
+    while(parentIndex >= 0){
+        if(heap[elementIndex] > heap[parentIndex]){
+            swap(elementIndex, parentIndex);
+            elementIndex = parentIndex;
+            parentIndex = parent(elementIndex);
+        }else{
+            break;
+        }
+    }
+}
+
+void insert(int value){
+    heap.push_back(value);
+    siftUp(heap.size()-1);
+}
+
+void display(){
+    cout<<"\n";
+    for(int elem: heap) cout<<elem<<" ";
+    cout<<"\n";
+}
+
+int main(){
+    insert(10);
+    insert(20);
+    insert(30);
+    insert(5);
+    insert(50);
+    display();
+}
