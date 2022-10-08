5751ea68cff6fb5c59e21c6a153f4d595c216676
ajmalmohad <ajmalajmal.2016@gmail.com>
2022-10-08 23:54:48 +0530

    Stat System Call

---
+++ os/stat.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/stat.h>
+int main(){
+        char path[10];
+        struct stat *nfile;
+        nfile = (struct stat*) malloc(sizeof(struct stat));
+        printf("Enter filename: ");
+        scanf("%s", path);
+        stat(path, nfile);
+        printf("User Id : %d\n", nfile->st_uid);
+        printf("Block Size : %ld\n", nfile->st_blksize);
+        printf("Access Time : %ld\n", nfile->st_atime);
+        printf("Mode : %d\n", nfile->st_mode);
+        printf("Size : %ld\n", nfile->st_nlink);
+}
