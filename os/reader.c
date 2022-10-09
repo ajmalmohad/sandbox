2bc32016ccb32101f5e11277d479d519829fbf94
ajmalmohad <ajmalajmal.2016@gmail.com>
2022-10-09 20:18:33 +0530

    Reader Writer IPC

---
+++ os/reader.c
@@ -0,0 +1,10 @@
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdio.h>
+
+int main(){
+    key_t key = 12345;
+    int shmid = shmget(key, 1024, 0666|IPC_CREAT);
+    char* str = shmat(shmid,0,0);
+    print("%s\n",str);
+}
