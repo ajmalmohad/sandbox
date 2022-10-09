ae80b28e1b3f02467d2b1776768426537ad49bfd
ajmalmohad <ajmalajmal.2016@gmail.com>
2022-10-09 16:46:35 +0530

    Wait Update

--- os/wait.c
+++ os/wait.c
@@ -1,28 +1,22 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <sys/wait.h>
 
 int main(){
         int pid, wid;
-        pid=fork();
+        pid = fork();
         if(pid==-1){
-                perror("Error Occured");
-                exit(0);
+                perror("Error Occured\n");
         }
         if(pid==0){
-                printf("Child Process Executing\n");
+                printf("Child Process Started\n");
                 sleep(2);
-                printf("Child Ended\n");
-                exit(0);
+                printf("Child Process Ended\n");
         }else{
-                printf("Parent Process Executing\n");
-                wid=wait(0);
-                if(wid==-1){
-                        perror("Parent Waiting");
-                }
-                printf("Parent Ended\n");
-                exit(0);
+                printf("Parent Process Started\n");
+                wid = wait(0);
+                if(wid==-1) perror("Parent Still Waiting");
+                printf("Parent Process Completed\n");
         }
 }
