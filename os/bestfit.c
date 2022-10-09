3a4737f1c33df9c2c655c398b821f98cd7961413
ajmalmohad <ajmalajmal.2016@gmail.com>
2022-10-09 23:47:59 +0530

    Worst Fit + Best Fit

--- os/bestfit.c
+++ os/bestfit.c
@@ -23,7 +23,7 @@ int main(){
     int count = 0;
     for (int i = 0; i < m; i++){
         allocated[i]=-1;
-        int difference = process[i];
+        int difference = blocks[i];
         for (int j = 0; j < n; j++){
             if(process[i]<=blocks[j]){
                 if(blocks[j]-process[i]<difference) {
